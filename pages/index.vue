87a6f395b03ac9badef9ecefa5b8e5ee2571a006
theotrc <theotricot12@gmail.com>
2023-10-04 18:03:41 +0200

    RIOTAPP

---
+++ pages/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    {{ info }}
+    
+    <input 
+      :value="username"
+      @input="event => this.username = event.target.value"/>
+      <button
+        @click="sumrequest(username)">Search</button>
+    <nuxt-link to="/"></nuxt-link>
+
+    <nuxt/>
+  </div>
+  
+</template>
+
+<script>
+import axios from "axios"
+export default {
+    data(){
+        return {
+            info: null,
+            username:null,
+
+        }
+    },
+
+
+    methods: {
+      mafonct(){ 
+        console.log() 
+      },
+      sumrequest(username) {
+        axios.get(`https://euw1.api.riotgames.com/tft/summoner/v1/summoners/by-name/${username}?api_key=${process.env.RIOT_KEY}`)
+          .then((response) => (this.info = response.data))
+          .then((response) => (console.log(response)))
+          .catch((error) => console.log(error))
+      }
+    }
+
+                }
+
+</script>
+<style>
+input{
+  color: white;
+}
+</style>
+
+ 
---
+++ pages/search.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        tft or lol
+        <v-responsive
+    class="mx-auto"
+    max-width="344"
+  >
+    <v-text-field
+      label="First name"
+      hide-details="auto"
+    ></v-text-field>
+  </v-responsive>
+    </div>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
